2506a85227f2ef3d23d3d94503c08daa50cd8521
Yves de Sá <yvesdsb98@gmail.com>
2022-04-17 20:51:13 -0300

    feat: creating koopa character already moving towards mario

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector(".mario");
+const background = document.querySelector(".background");
 
 let position = 30;
 let isJumping = false;
@@ -33,5 +34,33 @@ function jump() {
     }
   }, 50);
 };
+
+
+function createKoopa() {
+  const koopa = document.createElement('div');
+  let koopaPosition = 63;
+  let randomTime = Math.random() * 5000;
+
+  koopa.classList.add('koopa');
+  koopa.style.left = 63 + '%';
+  background.appendChild(koopa);
+
+  let leftInterval = setInterval(() => {
+    if (koopaPosition < 22) {
+      clearInterval(leftInterval);
+      background.removeChild(koopa);
+    } else if (koopaPosition > 0 && koopaPosition < 22) {
+      // clearInterval(leftInterval);
+      // document.body.innerHTML = '<h1 class="game-over" >Game Over</>'
+    } else {
+      koopaPosition -= 0.8;
+      koopa.style.left = koopaPosition + '%';
+    }
+  }, 20);
+
+  setTimeout(createKoopa, randomTime);
+}
+
+createKoopa();
 
 document.addEventListener('keyup', handleKeyUp);
