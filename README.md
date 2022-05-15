168446359ff64475aeb39936dfc8543cbec27204
Yves de Sá <yvesdsb98@gmail.com>
2022-05-15 19:02:57 -0300

    docs: created README

---
+++ README.md
@@ -0,0 +1,5 @@
+# desafio
+Criar uma cópia do jogo do dinossauro, famoso no Chrome por nos entreter quando estamos sem conexão.
+
+# tecnologia utilizadas
+HTML, CSS e JavaScript,
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const mario = document.querySelector(".mario");
 const background = document.querySelector(".background");
 
-let position = 30;
+let position = 33;
 let isJumping = false;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    if (!isJumping)
+    if (!isJumping) {
+      jumpAnimation();
       jump();
+    }
   };
 };
 
@@ -15,52 +17,78 @@ function jump() {
   isJumping = true;
 
   let upInterval = setInterval(() => {
-    if (position >= 50) {
+    if (position >= 49) {
       clearInterval(upInterval);
 
       let downInterval = setInterval(() => {
-        if (position <= 30) {
+        if (position <= 33) {
           clearInterval(downInterval);
           isJumping = false;
+          mario.classList.remove('mario-saltando');
+          mario.classList.add('mario-parado');
         } else {
-          position -= 5;
+          position -= 3;
           mario.style.bottom = position + '%';
         }
       }, 50);
 
     } else {
-      position += 5;
+      position += 3;
       mario.style.bottom = position + '%';
     }
   }, 50);
 };
 
 
-function createKoopa() {
-  const koopa = document.createElement('div');
-  let koopaPosition = 63;
+function createTube() {
+  const tube = document.createElement('div');
+  let tubePosition = 68;
   let randomTime = Math.random() * 5000;
 
-  koopa.classList.add('koopa');
-  koopa.style.left = 63 + '%';
-  background.appendChild(koopa);
+  tube.classList.add('tube');
+  tube.style.left = 68 + '%';
+  background.appendChild(tube);
 
   let leftInterval = setInterval(() => {
-    if (koopaPosition < 22) {
+    if (tubePosition < 20) {
       clearInterval(leftInterval);
-      background.removeChild(koopa);
-    } else if (koopaPosition > 25 && koopaPosition < 30 && position < 35) {
+      background.removeChild(tube);
+    } else if (tubePosition > 25 && tubePosition < 27.5 && position < 40) {
       clearInterval(leftInterval);
       document.body.innerHTML = '<div class="game-over"> <h2>Game Over</h2> </div>'
     } else {
-      koopaPosition -= 0.8;
-      koopa.style.left = koopaPosition + '%';
+      tubePosition -= 0.4;
+      tube.style.left = tubePosition + '%';
     }
   }, 20);
 
-  setTimeout(createKoopa, randomTime);
+  setTimeout(createTube, randomTime);
 }
 
-createKoopa();
+function walkingAnimation() {
+  setInterval(() => {
+    if (mario.classList.contains('mario-parado')) {
+      mario.classList.remove('mario-parado');
+      mario.classList.add('mario-andando');
+    } else {
+      mario.classList.remove('mario-andando');
+      mario.classList.add('mario-parado');
+    }
+  }, 200);
+}
+
+function jumpAnimation() {
+  if (mario.classList.contains('mario-parado')) {
+    mario.classList.remove('mario-parado');
+    mario.classList.add('mario-saltando');
+  } else {
+    mario.classList.remove('mario-andando');
+    mario.classList.add('mario-saltando');
+  }
+}
+
+createTube();
+
+walkingAnimation();
 
 document.addEventListener('keyup', handleKeyUp);
